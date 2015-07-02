b1daefb80f5e71f52bf39f71c5d29a46998f9eba
Andrew T. poe <techapoe@gmail.com>
2015-07-02 13:27:13 -0400

    Changed where file is installed

--- easy_git_installer.sh
+++ easy_git_installer.sh
@@ -4,17 +4,17 @@
 #*****************************************************************************#
 
 # First, cURL and install the actual easy_git script
-curl -o ~/bin/easy_git.sh https://raw.githubusercontent.com/andrewtpoe/git_automator/dev/easy_git.sh
+curl -o ~/usr/local/bin/easy_git.sh https://raw.githubusercontent.com/andrewtpoe/git_automator/dev/easy_git.sh
 
 # Perform the "first run" to acquire all of the dependencies needed
-eval ". ~/bin/easy_git.sh first_run"
+eval ". ~/usr/local/bin/easy_git.sh first_run"
 
 # Add the correct lines into that bash-profile
 echo "" >> ~/.bash_profile
 echo "##################################################" >> ~/.bash_profile
 echo "# This section adds the alias for easy_git" >> ~/.bash_profile
 echo "" >> ~/.bash_profile
-echo "alias easyg=". ~/bin/easy_git.sh"" >> ~/.bash_profile
+echo "alias easyg=". ~/usr/local/bin/easy_git.sh"" >> ~/.bash_profile
 echo "" >> ~/.bash_profile
 echo "##################################################" >> ~/.bash_profile
 
